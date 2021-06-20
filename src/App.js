1bc184d9c9e9edcfbb46956c88497f90c00e90cf
jinwoo <jinwoo73@gmail.com>
2021-06-20 16:01:30 +0900

    Rendering Movie app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,43 @@
 import React from "react";
+import axios from "axios";
+import Movie from "./Movie";
 
-function Potato(){
-  return <h1>I like Potato</h1>;
+class App extends React.Component {
+  state = {
+    isLoading: true,
+    movies: []
+  };
+  getMovies = async() => {
+    const {
+      data: {
+        data: {movies}
+      }
+    } = await axios.get(
+      "https://yts-proxy.now.sh/list_movies.json?sort_by=rating"
+    );
+    this.setState({movies,isLoading:false});
+  };
+  componentDidMount() {
+    this.getMovies();
+  }
+  render() {
+    const {isLoading,movies} = this.state;
+    return (
+      <div>
+        {isLoading
+          ? "Loading..."
+          : movies.map(movie => (
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+              />
+            ))}
+      </div>
+    );
+  }
 }
-
-function App() {
-  return (
-    <div>
-      <h1>Hello</h1>
-      <Potato />
-    </div>
-  );
-}
-
-export default App;
+export default App
